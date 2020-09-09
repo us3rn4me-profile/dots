e7d8b790ff147a54216da136b6ae49c2b064c0a9
us3rn4me <us3rn4me.profile@gmail.com>
2020-09-09 16:18:13 +0300

    Some changes on full_install

--- full_install.sh
+++ full_install.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
+# TextDecorations
 bold=$(tput bold)
 normal=$(tput sgr0)
 underline=$(tput smul)
 greencolor=$(tput setaf 2)
 
+# Functions
 echo -e "\n";
-function warning() {
+function warning() { # Check if user under root
 	echo "${bold}DO NOT START THIS SCRIPT AS SUDO
 	YOU CAN EXIT BY ENTERING CTRL-C FOR 10 SECONDS
 	IF EVERYTHING OK, THAN JUST GO ON${normal}"
-	sleep 10
+		sleep 10
 	}
 
-function install_yay() {
+function install_yay() { # Install yay
 	sudo pacman -Sy git base-devel
 	cd $HOME
 	git clone https://aur.archlinux.org/yay.git
@@ -21,58 +23,76 @@ function install_yay() {
 	makepkg -si
 	cd ../
 	rm -rf yay
-}
-
-function install_zsh() {
-	sudo pacman -Sy --noconfirm zsh
-	sudo chsh $USER -s /usr/bin/zsh
-	curl https://raw.githubusercontent.com/us3rn4me-profile/TheGreatestBSPWM/master/zshrc --output $HOME/.zshrc
 	yay_pack
 }
 
-function yay_pack() {
+function yay_pack() { # Install yay packages
 	yay -Sy google-chrome
 	yay -Sy polybar
+	yay -Sy typora
 }
 
-function GREATBSPWM() {
-	warning
-	cd $HOME
-	git clone https://github.com/us3rn4me-profile/TheGreatestBSPWM.git
-	cd TheGreatestBSPWM
-	# Config install
-	cp config $HOME/.config/ -r
-	# Wallpapers
-	mkdir $HOME/.wallpapers
-	unzip wallpapers/wallpapers.zip ~/.wallpapers/
-	# Xresources
-	cp Xresources_{day,night,morning} $HOME/
-	mv $HOME/{,.}Xresources_morning
-	mv $HOME/{,.}Xresources_day
-	mv $HOME/{,.}Xresources_night
+function install_zsh() { # Install zsh + oh-my-zsh
+	sudo pacman -Sy --noconfirm zsh
+	sudo chsh $USER -s /usr/bin/zsh
+	curl https://raw.githubusercontent.com/us3rn4me-profile/TheGreatestBSPWM/master/zshrc --output $HOME/.zshrc
 }
 
-function install_vim {
+function install_nvim {
 	warning
-	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	# Copying vimrc
-	cp vimrc $HOME/.vimrc
-	cp coc.vim $HOME/.coc.vim
+	sudo pacman -Sy python-pynvim neovim
+	sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+		https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+			# Copying nvimrc
+			cp config/nvim/ $HOME/.config/nvim -r
+		}
+
+	function init() { # Init copy of configurations
+		warning
+		cd $HOME
+		git clone https://github.com/us3rn4me-profile/dots.git
+		cd dots
+		# Config install TODO: Make config function
+		config
+		# Wallpapers
+		mkdir $HOME/.wallpapers
+		unzip wallpapers/wallpapers.zip ~/.wallpapers/
+		# Xresources
+		cp Xresources_{day,night,morning} $HOME/
+		mv $HOME/{,.}Xresources_morning
+		mv $HOME/{,.}Xresources_day
+		mv $HOME/{,.}Xresources_night
+	}
+
+function config() {
+	echo -e "Choose your config:\n1 - KDE;\n2 - BSPWM"
+	read userchoice
+	case $userchoice in
+		"1")
+		# KDE
+			sudo pacman -Sy pavucontrol p7zip unrar okular elisa xorg xclip xorg-xinit plasma dolphin ark konsole cmake powerdevil npm neofetch spectacle bluez bluez-utils task albert libunrar python
+		;;
+		"2")
+	# BSPWM
+		sudo pacman -Sy --noconfirm bspwm sxhkd rofi dunst xautolock lightdm lightdm-webkit2-greeter picom udiskie pavucontrol feh sxiv redshift kitty nvim ttf-jetbrains-mono ttf-lato fzf scrot ttf-font-awesome imagemagick nautilus alsa-lib alsa-utils xdg-user-dirs networkmanager ranger ffmpeg pulseaudio vlc materia-gtk-theme pulseaudio-alsa pulseaudio-bluetooth telegram-desktop raw-thumbnailer xarchiver zathura-djvu zathura-ps zathura-cb zathura-pdf-poppler zathura npm zip unzip python-pynvim ttf-dejavu xorg grub git fuse curl bluez bluez-utils tlp moc xclip flatpak
+		sudo systemctl enable lightdm.service # Window manager by default
+		sudo tlp start
+		# Copying config for TheGreatestBSPWM
+		cp config/{zathura,rofi,sxhkd,bpswm,dunst,picom.conf,kitty,ranger,mimeapps.list,zathura,redshift.conf} $HOME/.config/
+		;;
+	esac
 }
+
 
 if [[ -z $1 ]]; then
 	warning
-	sudo pacman -Sy --noconfirm bspwm sxhkd rofi dunst xautolock lightdm lightdm-webkit2-greeter picom udiskie pavucontrol feh sxiv redshift kitty vim ttf-jetbrains-mono ttf-lato fzf scrot ttf-font-awesome imagemagick nautilus alsa-lib alsa-utils xdg-user-dirs networkmanager ranger ffmpeg pulseaudio vlc materia-gtk-theme pulseaudio-alsa pulseaudio-bluetooth telegram-desktop raw-thumbnailer xarchiver zathura-djvu zathura-ps zathura-cb zathura-pdf-poppler zathura npm zip unzip python-pynvim ttf-dejavu xorg grub git fuse curl bluez bluez-utils tlp moc xclip flatpak
-
+	init
 	sudo systemctl enable NetworkManager.service # Network manager for wifi connecting
 	sudo systemctl enable bluetooth.service # Bluetooth connect
-	sudo systemctl enable lightdm.service # Window manager by default
-	sudo tlp start
 
 # Installing software and configurations
 install_yay
 install_zsh
-GREATBSPWM
 
 # Make default directories
 xdg-user-dirs-update
@@ -83,10 +103,10 @@ elif [[ "$1" = "-h" ]] || [[ "$1" = "--help" ]]; then
 	./full_install.sh [${underline}Function${normal}] - install part of config"
 
 	echo -e "\n${bold}Functions${normal}:
-	${greencolor}install_yay${normal} - install yay (with Google Chrome and Polybar)
-	${greencolor}install_zsh${normal} - install zsh config with oh-my-zsh
-	${greencolor}GREATBSPWM${normal}  - install TheGreatestBSPWM configuration
-	${greencolor}install_vim${normal} - install vim configuration"
+	${greencolor}install_yay${normal}  - install yay (with Google Chrome and Polybar)
+	${greencolor}install_zsh${normal}  - install zsh config with oh-my-zsh
+	${greencolor}init${normal}         - install configuration
+	${greencolor}install_nvim${normal} - install nvim configuration"
 
 else
 	$1
